aa96b87a093e16419b4fbf0a8b5510467d14063e
AmosHuKe <amoshuke@qq.com>
2019-06-10 17:48:51 +0800

    [新增] 我的（登陆，注册，找回密码）

---
+++ pages/mine/children/forget.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-white" :isBack="true" >
+			<block slot="backText"></block>
+			<block slot="content">找回密码</block>
+		</cu-custom>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/mine/children/register.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-white" :isBack="true" >
+			<block slot="backText"></block>
+			<block slot="content">注册</block>
+		</cu-custom>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
