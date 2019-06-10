9aaf7831f76501607960722a7eb91608da3196e8
AmosHuKe <amoshuke@qq.com>
2019-06-10 12:36:51 +0800

    [修改] 首页

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,62 +1,94 @@
 <template>
-  <view class="echarts-wrap">
-    <mpvue-echarts :echarts="echarts" :onInit="onInit" canvasId="demo-canvas" />
-  </view>
+	<view class="qiun-columns">
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt" >
+			<view class="qiun-title-dot-light">饼状图</view>
+		</view>
+		<view class="qiun-charts" >
+			<canvas canvas-id="canvasPie" id="canvasPie" class="charts" @touchstart="touchPie"></canvas>
+		</view>
+	</view>
 </template>
 
 <script>
-import echarts from 'lib/echarts/echarts.simple.min.js'
-import mpvueEcharts from 'lib/mpvue-echarts/src/echarts.vue'
-
-let chart = null;
-
-function initChart(canvas, width, height) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(chart);
-
-  var option = {
-	  title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            legend: {
-                data:['销量']
-            },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-  }; // ECharts 配置项
-
-  chart.setOption(option);
-
-  return chart; // 返回 chart 后可以自动绑定触摸操作
-}
-
-export default {
-  components: {
-    mpvueEcharts
-  },
-  data () {
-    return {
-      echarts,
-      onInit: initChart
-    }
-  }
-}
+	import uCharts from '../../lib/u-charts/u-charts.js'; 
+	var _self;
+	var canvaPie=null;
+   
+	export default {
+		data() {
+			return {
+				cWidth:'',
+				cHeight:'',
+				pixelRatio:1,
+				serverData:'',
+			}
+		},
+		mounted() {
+			_self = this;
+			this.cWidth=uni.upx2px(750);
+			this.cHeight=uni.upx2px(500);
+			this.getServerData();
+		},
+		methods: {
+			getServerData(){
+				uni.request({
+					url: 'https://www.easy-mock.com/mock/5cc586b64fc5576cba3d647b/uni-wx-charts/chartsdata2',
+					data:{
+					},
+					success: function(res) {
+						console.log(res.data.data)
+						let Pie={series:[]};
+						//这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
+						Pie.series=res.data.data.Pie.series;
+						_self.showPie("canvasPie",Pie);
+					},
+					fail: () => {
+						_self.tips="网络错误，小程序端请检查合法域名";
+					},
+				});
+			},
+			showPie(canvasId,chartData){
+				canvaPie=new uCharts({
+					$this:_self,
+					canvasId: canvasId,
+					type: 'pie',
+					fontSize:11,
+					legend:true,
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					series: chartData.series,
+					animation: true,
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					dataLabel: true,
+					extra: {
+						pie: {
+						  lableWidth:15
+						}
+					},
+				});
+			},
+			touchPie(e){
+				canvaPie.showToolTip(e, {
+					format: function (item) {
+						return item.name + ':' + item.data 
+					}
+				});
+			},
+		}
+	}
 </script>
 
-<style scoped>
-.echarts-wrap {
-  width: 100%;
-  height: 300px;
-}
+<style>
+page{background:#F2F2F2;width: 750upx;overflow-x: hidden;}
+.qiun-padding{padding:2%; width:96%;}
+.qiun-wrap{display:flex; flex-wrap:wrap;}
+.qiun-rows{display:flex; flex-direction:row !important;}
+.qiun-columns{display:flex; flex-direction:column !important;}
+.qiun-common-mt{margin-top:10upx;}
+.qiun-bg-white{background:#FFFFFF;}
+.qiun-title-bar{width:96%; padding:10upx 2%; flex-wrap:nowrap;}
+.qiun-title-dot-light{border-left: 10upx solid #0ea391; padding-left: 10upx; font-size: 32upx;color: #000000}
+.qiun-charts{width: 750upx; height:500upx;background-color: #FFFFFF;}
+.charts{width: 750upx; height:500upx;background-color: #FFFFFF;}
 </style>
